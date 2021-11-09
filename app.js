852316737d20372719859f7b055c9640347a3368
Pedro Gancho <pedro.gancho@inlifeportugal.com>
2021-11-09 17:53:40 +0000

    Solved Lab Elon Musk

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+// Middleware that serves files from the /public folder
+app.use(express.static("public"));
+
+// GET  /
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/views/index.html");
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/public/views/about.html");
+});
+
+app.get("/works", (req, res) => {
+    res.sendFile(__dirname + "/public/views/works.html");
+  });
+
+app.listen(3000, () => {
+  console.log("Server is running!");
+});
